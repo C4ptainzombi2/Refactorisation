f25f4e4858a8f82a1edf03cf95467f7caa5ba5b2
C4ptainzombi <C4ptainzombi@gmail.com>
2025-11-09 20:55:50 +0100

    dd

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// config.php
-define('BASE_PATH', __DIR__);
-define('DATA_PATH', BASE_PATH . '/data');
-define('REGIONS_PATH', DATA_PATH . '/regions');
-define('API_URL', 'https://script.google.com/macros/s/AKfycby5A8umWHXsxqHjSOQS6y6J3n-Kijdbj0g6uZyBkCmNl5niD5FcvM_Z7JiPwpQgZ9eT5A/exec');
-define('JSON_FILE', DATA_PATH . '/structures.json');
-define('SITE_URL', 'https://botdiscordng.ovh/PHStructures');
-?>
